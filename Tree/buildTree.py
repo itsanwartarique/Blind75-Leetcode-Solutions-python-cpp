2ff785102a9bafb17e6d3521a9c15668fa18422a
itsanwartarique <anwartarique1121@gmail.com>
2022-08-01 15:38:19 +0530

    added buildTree.py

---
+++ Tree/buildTree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
+            
+        rootData = preorder[0]
+        print(rootData)
+        mid = inorder.index(rootData)
+        
+        root = TreeNode(rootData)
+        root.left = self.buildTree(preorder[1:mid+1],inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:],inorder[mid+1:])
+        return root
+        
+
+        
