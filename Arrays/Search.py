9825e092e610a42957b354d9321916141bfda76b
itsanwartarique <anwartarique1121@gmail.com>
2022-08-10 12:15:32 +0530

    added Search.py

---
+++ Arrays/Search.py
@@ -0,0 +1,20 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums)-1
+        while(l<=r):
+            mid = (l+r)//2
+            if(nums[mid] == target):
+                return mid
+            # left sorted array
+            if(nums[l]<=nums[mid]):
+                if(target<nums[l] or target>nums[mid]):
+                    l = mid+1
+                else:
+                    r = mid-1
+            # right sorted array
+            else:
+                if(target<nums[mid] or target>nums[r]):
+                    r = mid-1
+                else:
+                    l = mid+1
+        return -1
